dba0fe33b87fed4c1acf0d0cd1d844c448415b38
ustsepavy <vladstepovoyvios@gmail.com>
2021-03-24 22:19:44 +0300

    feat: added base structure

---
+++ src/components/Home/Home.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function Header(): JSX.Element {
+  return <div className="Home">Home</div>;
+}
+
+export default Header;
---
+++ src/components/MiniGames/MiniGames.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function MiniGames(): JSX.Element {
+  return <div className="MiniGames">MiniGames</div>;
+}
+
+export default MiniGames;
