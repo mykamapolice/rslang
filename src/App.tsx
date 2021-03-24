c9b5bd7bb1147509f40487c7127c81c824d2b038
Ilya Barachenia <ilya.barachenia@gmail.com>
2021-03-24 14:29:23 +0100

    init: create Webpack with React and TS

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.css';
+import './App.css';
+
+function App():JSX.Element {
+  return (
+    <div className="App">
+      Вечер в хату
+    </div>
+  );
+}
+
+export default App;
