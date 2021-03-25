d9322a7fb43152bfe1609bb820162b3a3f453274
Ilya Barachenia <ilya.barachenia@gmail.com>
2021-03-25 14:41:23 +0100

    feat: add settings components

---
+++ src/components/Settings/SoundSettings/SoundSettings.tsx
@@ -0,0 +1,62 @@
+import React from 'react';
+import { Form } from 'react-bootstrap';
+import styles from './SoundSettings.module.css';
+
+const SoundSettings = (): JSX.Element => (
+    <div key="custom-checkbox" className={`${styles.soundSettingsContainer} mb-3`}>
+
+      <div>
+        <Form.Group
+          controlId="music-volume"
+          onChange={(e: React.FormEvent<HTMLInputElement>) => console.log(e.target)}
+        >
+
+          <Form.Label><h3>Настройки музыки</h3></Form.Label>
+          <div className={styles.soundSettingItemsContainer}>
+            <div style={{ margin: '1rem' }}>
+              <Form.Check
+                type="switch"
+                id="custom-switch-music"
+                label="Музыка выкл/вкл"
+                // checked={settings.musicOn}
+                // onChange={turnOnOffMusicHandler}
+              />
+            </div>
+            <div style={{ margin: '1rem' }}>
+              <Form.Control
+                type="range"
+                // defaultValue={settings.musicVolume}
+              />
+            </div>
+          </div>
+        </Form.Group>
+      </div>
+
+      <div>
+        <Form.Group
+          controlId="sound-volume"
+          onChange={(e: React.FormEvent<HTMLInputElement>) => console.log(e.target)}
+        >
+          <Form.Label><h3>Настройки звука</h3></Form.Label>
+          <div className={styles.soundSettingItemsContainer}>
+            <div style={{ margin: '1rem' }}>
+              <Form.Check
+                type="switch"
+                id="custom-switch-sound"
+                label="Звуки выкл/вкл"
+                // checked={settings.soundOn}
+                // onChange={turnOnOffSoundHandler}
+              />
+            </div>
+            <Form.Control
+              type="range"
+              // defaultValue={settings.soundVolume}
+            />
+          </div>
+        </Form.Group>
+      </div>
+
+    </div>
+);
+
+export default SoundSettings;
