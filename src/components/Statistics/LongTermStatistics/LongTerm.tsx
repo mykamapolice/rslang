6956ec16bf636f574ab6ef48bb223f0229f07cdb
Ilya Barachenia <ilya.barachenia@gmail.com>
2021-03-26 14:41:47 +0100

    feat: add examples of charts

--- src/components/Statistics/LongTermStatistics/LongTerm.tsx
+++ src/components/Statistics/LongTermStatistics/LongTerm.tsx
@@ -1,19 +1,101 @@
 import React from 'react';
 import {
-  VictoryBar, VictoryGroup, VictoryChart, VictoryArea, VictoryStack,
+  VictoryChart,
+  VictoryArea,
+  VictoryStack,
+  VictoryLine,
+  createContainer,
 } from 'victory';
 
-const LongTermStatistics = () => (
-    <div>
-      <VictoryStack>
-        <VictoryArea
-          data={[{ x: 2, y: 2 }, { x: 3, y: 3 }, { x: 4, y: 5 }]}
-        />
-        <VictoryArea
-          data={[{ x: 1, y: 1 }, { x: 2, y: 4 }, { x: 2, y: 5 }]}
-        />
-      </VictoryStack>
-    </div>
-);
+const LongTermStatistics = () => {
+  const VictoryZoomVoronoiContainer = createContainer('zoom', 'voronoi');
+  const data = [
+    {
+      x: 1,
+      y: 2,
+    },
+    {
+      x: 2,
+      y: 3,
+    },
+    {
+      x: 3,
+      y: 5,
+    },
+    {
+      x: 4,
+      y: 4,
+    },
+    {
+      x: 5,
+      y: 7,
+    },
+  ];
 
+  return (
+    <div style={{ width: '75%', margin: '0 auto' }}>
+      <VictoryChart
+        domainPadding={{ y: 10 }}
+        containerComponent={(
+          <VictoryZoomVoronoiContainer
+            // @ts-ignore
+            labels={({ datum }: number) => datum.y}
+          />
+        )}
+      >
+        <VictoryLine
+          style={{
+            data: { stroke: '#c43a31' },
+            parent: { border: '1px solid #ccc' },
+          }}
+          data={data}
+        />
+      </VictoryChart>
+
+      <div>
+        <VictoryChart
+          containerComponent={(
+            <VictoryZoomVoronoiContainer
+              // @ts-ignore
+              labels={({ datum }: number) => datum.y}
+            />
+          )}
+        >
+          <VictoryLine
+            data={data}
+          />
+        </VictoryChart>
+      </div>
+
+      <VictoryChart>
+        <VictoryStack>
+          <VictoryArea
+            data={[{
+              x: 2,
+              y: 2,
+            }, {
+              x: 3,
+              y: 3,
+            }, {
+              x: 5,
+              y: 5,
+            }]}
+          />
+          <VictoryArea
+            data={[{
+              x: 1,
+              y: 2,
+            }, {
+              x: 3,
+              y: 4,
+            }, {
+              x: 5,
+              y: 9,
+            }]}
+          />
+        </VictoryStack>
+      </VictoryChart>
+    </div>
+  );
+};
 export default LongTermStatistics;
