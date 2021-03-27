e05426630e7da4dc44817e7dac60d5a15f034297
Ilya Barachenia <ilya.barachenia@gmail.com>
2021-03-27 22:37:24 +0100

    feat: add validation to Authentication

--- src/components/Authentication/Authentication.tsx
+++ src/components/Authentication/Authentication.tsx
@@ -6,6 +6,7 @@ const Authentication: FC = (): JSX.Element => {
   const [email, setEmail] = useState('');
   const [name, setName] = useState('');
   const [password, setPassword] = useState('');
+  const [validated, setValidated] = useState(false);
 
   const handleEmailChange =  (event: React.ChangeEvent<HTMLInputElement>) => {
     setEmail(event.target.value)
@@ -22,8 +23,13 @@ const Authentication: FC = (): JSX.Element => {
 
   const handleSubmit = (event:React.FormEvent) => {
     console.log(name, password, email)
-    event.preventDefault()
-    event.stopPropagation()
+    const form: any = event.currentTarget;
+    console.log(form.checkValidity())
+    if (form.checkValidity() === false) {
+      event.preventDefault();
+      event.stopPropagation();
+    }
+    setValidated(true);
   };
 
   return (
@@ -36,27 +42,36 @@ const Authentication: FC = (): JSX.Element => {
         show={show}
         onHide={handleClose}
         backdrop="static"
-        keyboard={false}
+        keyboard={true}
       >
         <Modal.Header closeButton>
           <Modal.Title>Rs-lang</Modal.Title>
         </Modal.Header>
         <Modal.Body>
-          <Form onSubmit={handleSubmit}>
+          <Form noValidate validated={validated} onSubmit={handleSubmit}>
             <Form.Group controlId="formBasicEmail">
               <Form.Label>Почта</Form.Label>
-              <Form.Control type="email" placeholder="Enter email" value={email} onChange={handleEmailChange}/>
+              <Form.Control type="email" placeholder="Enter email" value={email} onChange={handleEmailChange} required/>
+              <Form.Control.Feedback type="invalid">
+                Введите корректную почту
+              </Form.Control.Feedback>
               <Form.Text className="text-muted">
                 Мы не передадим ваш email 3 лицам
               </Form.Text>
             </Form.Group>
             <Form.Group controlId="formBasicPassword">
               <Form.Label>Пароль</Form.Label>
-              <Form.Control type="password" placeholder="Password"  value={password} onChange={handlePasswordChange}/>
+              <Form.Control type="password" placeholder="Password"  value={password} onChange={handlePasswordChange} required/>
+              <Form.Control.Feedback type="invalid">
+                Введите пароль
+              </Form.Control.Feedback>
             </Form.Group>
             <Form.Group controlId="formBasicName">
               <Form.Label>Имя</Form.Label>
-              <Form.Control type="name" placeholder="Name"  value={name} onChange={handleNameChange}/>
+              <Form.Control type="name" placeholder="Name"  value={name} onChange={handleNameChange} required/>
+              <Form.Control.Feedback type="invalid">
+                Введите имя
+              </Form.Control.Feedback>
             </Form.Group>
             <div style={{display: 'flex', justifyContent: 'space-around'}}>
               <Button variant="primary" type="submit" >
